5c8e58de5ba0ed3a36e5491de7a9ebf462fef5c5
cmthomas000 <cmthomas000@users.noreply.github.com>
2016-09-15 12:10:44 -0600

    Huge change in design, but it passed off
    Took out the state machine and just used interrupts to keep track of time.

--- lab2/lab2code/lab2_0/src/main.c
+++ lab2/lab2code/lab2_0/src/main.c
@@ -6,8 +6,7 @@
 
 
 #define PB_DEBOUNCE_TIME 5 //timer ticks at 10ms, so 5 = 50ms
-#define PRE_AUTO_TIMER_MAX 100 // hold button for 1s
-#define AUTO_TIMER_MAX 50 // increment 2x per s
+#define HALF_TIMER_SEC 50 // increment 2x per s
 #define TIMER_SEC 100
 #define NUMBER_OF_BUTTONS 5
 // This is an example of an enumerated type I like to use for state machines. Here for reference...
@@ -17,7 +16,6 @@ enum buttonHandler_st_t {
 	wait_st,
 	hold_st,
 	auto_rate_st,
-	final_st,
 } buttonState[NUMBER_OF_BUTTONS] = {init_st, init_st, init_st, init_st, init_st};
 
 
@@ -49,7 +47,21 @@ XGpio gpPB;   // This is a handle for the push-button GPIO block.
 int hour = 0;
 int minute = 0;
 int second = 0;
+
+int secondCount = 0;
+int halfSecondCount = 0;
+int debounceCount = 0;
 bool scrollDisable = FALSE;
+
+// Flags for the buttons
+bool secondFlag = FALSE;
+bool halfSecondFlag = FALSE;
+bool debounceFlag = FALSE;
+bool dirButtonHeld = FALSE;
+bool autoIncrementFlag = FALSE;
+//bool button_pressed() {
+//	return buttonDebounced;
+//}
 
 void increment_clock() {
 	if(!buttonStateReg) { // only increment time when not setting clock
@@ -75,6 +87,7 @@ void print_clock() {
 	xil_printf("\r%02d:%02d:%02d",hour,minute,second);
 }
 
+// Increments or decrements the saved time values
 void reset_time() {
 	bool time_changed = FALSE;
 	if(buttonStateReg & BTN_MASKS[BTN_HR]) { // SET HOUR
@@ -118,112 +131,95 @@ void reset_time() {
 	if(time_changed) print_clock();
 }
 
+// Checks to see if any button is pressed at all
+bool button_pressed() {
+	return buttonStateReg>0;
+}
 
+// This function is used to distinguish the up and down buttons from the rest
+bool inc_button_pressed() {
+	return (buttonStateReg & BTN_MASKS[BTN_UP]) || (buttonStateReg & BTN_MASKS[BTN_DOWN]);
+}
+
+// This function is used to distinguish the hour, minute and second buttons from the rest
+bool time_button_pressed() {
+	return (buttonStateReg & BTN_MASKS[BTN_HR]) || (buttonStateReg & BTN_MASKS[BTN_MIN]) || (buttonStateReg & BTN_MASKS[BTN_SEC]);
+}
 // This is invoked in response to a timer interrupt.
 // It does 2 things: 1) debounce switches, and 2) advances the time.
 void timer_interrupt_handler() {
-	int i;
 	bool time_set = FALSE;
 
-	int debounce_timer[NUMBER_OF_BUTTONS];
-	int pre_auto_timer[NUMBER_OF_BUTTONS];
-	int auto_timer[NUMBER_OF_BUTTONS];
+	secondFlag = FALSE;
+	halfSecondFlag = FALSE;
 
-	if(timerCount>=TIMER_SEC) {
-		increment_clock();
+	// Increment counters for the flags
+	secondCount++;
+	halfSecondCount++;
+
+	// Second counter
+	if(secondCount >= TIMER_SEC && !button_pressed()) {
+		secondCount = 0;
+		dirButtonHeld = FALSE;
+		secondFlag = TRUE;
 		print_clock();
+		increment_clock();
 
-		timerCount=0;
+	} else if (secondCount >= TIMER_SEC && dirButtonHeld) {
+		secondCount = 0;
+		secondFlag = TRUE;
+		autoIncrementFlag = TRUE;
+		halfSecondCount = 0;
+		print_clock();
+		reset_time();
+	}
+
+	// The half second counter
+		if(halfSecondCount >= HALF_TIMER_SEC) {
+			halfSecondCount = 0;
+			halfSecondFlag = TRUE;
+		}
+
+	// Auto increment counter
+	if(halfSecondCount >= HALF_TIMER_SEC && autoIncrementFlag) {
+		print_clock();
+		reset_time();
+		halfSecondCount = 0;
+	}
+
+
+
+	// Button debounce logic
+	if(debounceCount >= PB_DEBOUNCE_TIME && !debounceFlag) {
+		debounceCount = 0;
+		secondCount = 0;
+		debounceFlag = TRUE;
+		reset_time();
+	}
+
+	// Debounce the buttons
+	if(button_pressed()) {
+		debounceCount++;
 	} else {
-		timerCount++;
+		debounceCount = 0;
+		debounceFlag = FALSE;
 	}
 
-	for(i = 0; i < NUMBER_OF_BUTTONS; i++) {
-		//print("*");
-	// switch for state actions
-	switch(buttonState[i]) {
-	case init_st:
-		break;
-	case no_touch_st:
-		break;
-	case wait_st:
-		debounce_timer[i]++;
-		break;
-	case hold_st:
-		if(i == BTN_UP || i == BTN_DOWN) {
-			pre_auto_timer[i]++;
-		}
-		break;
-	case auto_rate_st:
-		auto_timer[i]++;
-		break;
-	case final_st:
-		//debounce_timer[i]++;
-		break;
+
+	// Check to see if the button was held for s second
+	if(!dirButtonHeld && debounceFlag && inc_button_pressed() && time_button_pressed()) {
+		reset_time();
+		print_clock();
+		dirButtonHeld = TRUE;
+		secondCount = 0;
 	}
 
-	//switch for state transitions,
-	// including mealy actions.
-	switch(buttonState[i]) {
-	case init_st:
-		debounce_timer[i] = 0;
-		buttonState[i] = no_touch_st;
-		pre_auto_timer[i] = 0;
-		auto_timer[i] = 0; // starting at max so that it will change time, then wait 0.5s
-		break;
-	case no_touch_st:
-		if(buttonStateReg & BTN_MASKS[i]) // some button is pushed
-			buttonState[i] = wait_st;
-		break;
-	case wait_st:
-
-		if(debounce_timer[i] >= PB_DEBOUNCE_TIME) {// timer expired
-			buttonState[i] = hold_st;
-			debounce_timer[i] = 0;
-			//xil_printf("%d",buttonStateReg);
-			if(!time_set) {
-				reset_time();
-				time_set = TRUE; // makes sure that reset_time is called only once per interrupt cycle
-			}
-		// mealy action to increment/decrement values
-		}
-		else if(!(buttonStateReg & BTN_MASKS[i])) {
-			debounce_timer[i] = 0;
-			buttonState[i] = no_touch_st;
-		}
-		break;
-	case hold_st:
-
-		if(pre_auto_timer[i] >= PRE_AUTO_TIMER_MAX && (i == BTN_UP || i == BTN_DOWN)) { // only up and down should trigger hold actions.
-			pre_auto_timer[i] = 0;
-			buttonState[i] = auto_rate_st;
-		}
-		if(!(buttonStateReg & BTN_MASKS[i])) // the button is no longer pushed
-			buttonState[i] = final_st;
-		break;
-	case auto_rate_st:
-		print("auto rate\n");
-
-		if(auto_timer[i] >= AUTO_TIMER_MAX) {
-
-			auto_timer[i] = 0;
-			if(!time_set) {
-				reset_time();
-				//xil_printf("change btn %d\n\r",i);
-				time_set = TRUE;
-			}
-		}
-		if(!(buttonStateReg & BTN_MASKS[i])) {
-			buttonState[i] = final_st;
-		}
-	case final_st:
-		debounce_timer[i] = 0;
-		pre_auto_timer[i] = 0;
-		auto_timer[i] = 0; // starting at max so that it will change time, then wait 0.5s
-		buttonState[i] = no_touch_st;
-		break;
+	if(!inc_button_pressed()) {
+		dirButtonHeld = FALSE;
+		autoIncrementFlag = FALSE;
 	}
-	}
+
+
 
 }
 
