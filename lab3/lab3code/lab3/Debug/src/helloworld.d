e055135364f01b8b4c8a53fc48bd5b44da7a73f8
cmthomas000 <superman@CB400-07.WORKGROUP>
2016-09-21 12:13:14 -0600

    hdmi hello world

---
+++ lab3/lab3code/lab3/Debug/src/helloworld.d
@@ -0,0 +1,42 @@
+src/helloworld.d src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+  ../src/platform_config.h \
+  ../../lab3_bsp/microblaze_0/include/xparameters.h \
+  ../../lab3_bsp/microblaze_0/include/xaxivdma.h \
+  ../../lab3_bsp/microblaze_0/include/xaxivdma_hw.h \
+  ../../lab3_bsp/microblaze_0/include/xil_types.h \
+  ../../lab3_bsp/microblaze_0/include/xil_io.h \
+  ../../lab3_bsp/microblaze_0/include/mb_interface.h \
+  ../../lab3_bsp/microblaze_0/include/xbasic_types.h \
+  ../../lab3_bsp/microblaze_0/include/xaxivdma_i.h \
+  ../../lab3_bsp/microblaze_0/include/xdebug.h \
+  ../../lab3_bsp/microblaze_0/include/xstatus.h \
+  ../../lab3_bsp/microblaze_0/include/xil_assert.h \
+  ../../lab3_bsp/microblaze_0/include/xio.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../lab3_bsp/microblaze_0/include/xparameters.h:
+
+../../lab3_bsp/microblaze_0/include/xaxivdma.h:
+
+../../lab3_bsp/microblaze_0/include/xaxivdma_hw.h:
+
+../../lab3_bsp/microblaze_0/include/xil_types.h:
+
+../../lab3_bsp/microblaze_0/include/xil_io.h:
+
+../../lab3_bsp/microblaze_0/include/mb_interface.h:
+
+../../lab3_bsp/microblaze_0/include/xbasic_types.h:
+
+../../lab3_bsp/microblaze_0/include/xaxivdma_i.h:
+
+../../lab3_bsp/microblaze_0/include/xdebug.h:
+
+../../lab3_bsp/microblaze_0/include/xstatus.h:
+
+../../lab3_bsp/microblaze_0/include/xil_assert.h:
+
+../../lab3_bsp/microblaze_0/include/xio.h:
