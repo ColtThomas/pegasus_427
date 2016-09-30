0cdbda5bb4fa518cc222c3f1784dd59b9aced311
cmthomas000 <cmthomas000@users.noreply.github.com>
2016-09-29 21:03:16 -0600

    Commenting stuff

--- lab3/lab3code/lab3/src/bunkers.c
+++ lab3/lab3code/lab3/src/bunkers.c
@@ -17,6 +17,7 @@
 #define packword6(b5,b4,b3,b2,b1,b0) \
 		((b5  << 5 ) | (b4  << 4 ) | (b3  << 3 ) | (b2  << 2 ) | (b1  << 1 ) | (b0  << 0 ))
 
+// Constants dealing with the bunker positioning
 #define BUNKER_MARGIN_BOTTOM 60
 #define BUNKER_MARGIN_RIGHT 60
 #define BUNKER_WIDTH 24
@@ -35,6 +36,7 @@
 #define BUNKER_FOUR_LEFT_BOUND BUNKER_SPACING*4 + BUNKER_WIDTH*3
 #define BUNKER_FOUR_RIGHT_BOUND BUNKER_SPACING*4 + BUNKER_WIDTH*4
 
+// More bunker css stuff for easy spacing and quadrant definitions
 #define BUNKER_QUAD_WIDTH BUNKER_WIDTH/3
 #define BUNKER_QUAD_HEIGHT BUNKER_HEIGHT/3
 #define BUNKER_QUAD_X_1 0
@@ -44,6 +46,17 @@
 #define BUNKER_QUAD_Y_2 BUNKER_QUAD_HEIGHT
 #define BUNKER_QUAD_Y_3 BUNKER_QUAD_HEIGHT*2
 
+#define BUNKER_QUAD_ZERO 0
+#define BUNKER_QUAD_ONE 1
+#define BUNKER_QUAD_TWO 2
+#define BUNKER_QUAD_THREE 3
+#define BUNKER_QUAD_FOUR 4
+#define BUNKER_QUAD_FIVE 5
+#define BUNKER_QUAD_SIX 6
+#define BUNKER_QUAD_SEVEN 7
+#define BUNKER_QUAD_EIGHT 8
+#define BUNKER_QUAD_NINE 9
+#define BUNKER_QUAD_VOID 0
 // Shape of the entire bunker.
 static const int bunker_24x18[] =
 {
@@ -114,15 +127,20 @@ u32 bunkerStatus[4][9] = { // global values to change
 		{0,0,0,0,0,0,0,4,0}
 };
 
+// Initialization function for the bunkers
 void bunkers_draw_initial() {
 	int i;
 	int x, y;
 	int xOffset;
 	int yOffset;
+	// Iterate though all 4 bunkers
 	for(i = 0; i < GLOBALS_NUMBER_OF_BUNKERS; i++) {
+		
+		//Trace the x and y coordinates of the bitmaps
 		for(x = 0; x < BUNKER_WIDTH; x++) {
 			for(y = 0; y < BUNKER_HEIGHT; y++) {
-				if(bunker_24x18[y] & (1 << x)) {
+				if(bunker_24x18[y] & (1 << x)) { // If the coordinate matches the bitmap, we draw
+					// Offset puts the bunkers right where we want them, evenly spaced like a champ
 					xOffset = x + BUNKER_SPACING + i*BUNKER_SPACING + i* BUNKER_WIDTH;
 					yOffset = y + SCREEN_HEIGHT - BUNKER_MARGIN_BOTTOM;
 					screen_draw_double_pixel(xOffset,yOffset,SCREEN_GREEN);
@@ -135,15 +153,15 @@ void bunkers_draw_initial() {
 
 // Might want to consider splitting this to update a specific bunker to save render time
 void bunkers_update() {
+		// We do a lot of iteration though each bunker and the quadrants of each one
 		int i,j;
 		int x, y;
 		int xOffset,yOffset,xQuad,yQuad,quadIndex,quadDamage = 0;
-
+		// Iterate though each bunker
 		for(i = 0; i < GLOBALS_NUMBER_OF_BUNKERS; i++) {
-			xil_printf("\r\nBunker %d",i);
 			for(y = 0; y < BUNKER_HEIGHT; y++) {
 				for(x = 0; x < BUNKER_WIDTH; x++) {
-					// Figure out how to distinguish a quadrant
+					// Get the positioning of each bunker
 					xOffset = x + BUNKER_SPACING + i*BUNKER_SPACING + i* BUNKER_WIDTH;
 					yOffset = y + SCREEN_HEIGHT - BUNKER_MARGIN_BOTTOM;
 
@@ -152,52 +170,43 @@ void bunkers_update() {
 					yQuad = y;
 
 
-					// Quadrant indicator
+					// Each bunker is divided into 9 quadrants. These quadrants will help us apply
+					// damage to each patch on the bunker as they get hit.
 					if((xQuad>=BUNKER_QUAD_X_1) & (xQuad<BUNKER_QUAD_X_2)) {
 						if((yQuad>=BUNKER_QUAD_Y_1) & (yQuad<BUNKER_QUAD_Y_2)) {
-							quadIndex = 0;
-//							xil_printf("\r\nquad0");
+							quadIndex = BUNKER_QUAD_ZERO;
 						}
 						else if((yQuad>=BUNKER_QUAD_Y_2) & (yQuad<BUNKER_QUAD_Y_3)) {
-							quadIndex = 3;
-//							xil_printf(".");
+							quadIndex = BUNKER_QUAD_THREE;
 						}
 						else if((yQuad>=BUNKER_QUAD_Y_3) & (yQuad<BUNKER_QUAD_Y_3+BUNKER_QUAD_HEIGHT)) {
-							quadIndex = 6;
-//							xil_printf(".");
+							quadIndex = BUNKER_QUAD_SIX;
 						}
 					}
 					else if((xQuad>=BUNKER_QUAD_X_2) & (xQuad<BUNKER_QUAD_X_3)) {
 						if((yQuad>=BUNKER_QUAD_Y_1) & (yQuad<BUNKER_QUAD_Y_2)) {
-							quadIndex = 1;
-//							xil_printf(".");
+							quadIndex = BUNKER_QUAD_ONE;
 						}
 						else if((yQuad>=BUNKER_QUAD_Y_2) & (yQuad<BUNKER_QUAD_Y_3)) {
-							quadIndex = 4;
-//							xil_printf(".");
+							quadIndex = BUNKER_QUAD_FOUR;
 						}
 						else if((yQuad>=BUNKER_QUAD_Y_3) & (yQuad<BUNKER_QUAD_Y_3+BUNKER_QUAD_HEIGHT)) {
-							quadIndex = 7;
-//							xil_printf(".");
+							quadIndex = BUNKER_QUAD_SEVEN;
 						}
 					}
 					else if((xQuad>=BUNKER_QUAD_X_3) & (xQuad<BUNKER_QUAD_X_3+BUNKER_QUAD_WIDTH)) {
 						if((yQuad>=BUNKER_QUAD_Y_1) & (yQuad<BUNKER_QUAD_Y_2)) {
-							quadIndex = 2;
-//							xil_printf(".");
+							quadIndex = BUNKER_QUAD_TWO;
 						}
 						else if((yQuad>=BUNKER_QUAD_Y_2) & (yQuad<BUNKER_QUAD_Y_3)) {
-							quadIndex = 5;
-//							xil_printf(".");
+							quadIndex = BUNKER_QUAD_FIVE;
 						}
 						else if((yQuad>=BUNKER_QUAD_Y_3) & (yQuad<BUNKER_QUAD_Y_3+BUNKER_QUAD_HEIGHT)) {
-							quadIndex = 8;
-//							xil_printf(".");
+							quadIndex = BUNKER_QUAD_EIGHT;
 						}
 					}
 					else {
-						quadIndex = 0;
-//						xil_printf("-");
+						quadIndex = BUNKER_QUAD_VOID;
 					}
 
 					quadDamage = bunkerStatus[i][quadIndex]; // Indicates which damage block to render
