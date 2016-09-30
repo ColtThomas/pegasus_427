3fe2d401747eaec405c9b8ce91ee6d066f72f4bd
cmthomas000 <cmthomas000@users.noreply.github.com>
2016-09-29 21:27:40 -0600

    Function descriptions added

--- lab3/lab3code/lab3/src/text.h
+++ lab3/lab3code/lab3/src/text.h
@@ -11,7 +11,10 @@
 #include "screen.h"
 #include "globals.h"
 
+// Used to write a char to the game screen at given coordinate
 void text_write(unsigned char val, point_t coord);
+
+// Draws the scoreboard values to the top of the screen
 void text_draw_score();
 
 #endif /* TEXT_H_ */
