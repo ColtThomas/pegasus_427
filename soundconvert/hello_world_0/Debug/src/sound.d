20bd8f661a3a7982505e527880c16df8c8e237ec
cmthomas000 <cmthomas000@gmail.com>
2016-10-25 00:29:59 -0600

    got rid of stuff in the project folder; having a ram issue

--- soundconvert/hello_world_0/Debug/src/sound.d
+++ soundconvert/hello_world_0/Debug/src/sound.d
@@ -1,3 +1,15 @@
-src/sound.d src/sound.o: ../src/sound.c ../src/sound/explosion.h
+src/sound.d src/sound.o: ../src/sound.c ../src/sound/explosion.h \
+  ../src/xac97_l.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xbasic_types.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xio.h \
+  ../../hello_world_bsp_0/microblaze_0/include/xbasic_types.h
 
 ../src/sound/explosion.h:
+
+../src/xac97_l.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xbasic_types.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xio.h:
+
+../../hello_world_bsp_0/microblaze_0/include/xbasic_types.h:
--- soundconvert/hello_world_0/src/sound.c
+++ soundconvert/hello_world_0/src/sound.c
@@ -5,6 +5,8 @@
  *      Author: superman
  */
 #include "sound/explosion.h"
+#include "xac97_l.h"
+
 // Plays the given .wav file.
 
 
@@ -20,5 +22,21 @@ void sound_play() {
 }
 
 void run_sound_test() {
+	// Hard reset done in the main
 
+	// await the codec
+	XAC97_AwaitCodecReady(XPAR_AUDIO_CODEC_BASEADDR);
+	// enable the VRA
+
+	// Set the frequency
+
+	// Set the volume
+
+	//clear FIFO
+
+	// set control bit to cause the IN_FIFO interrupts
+
+	//add AC97 mask to general INT masks
+
+	sound_play();
 }
